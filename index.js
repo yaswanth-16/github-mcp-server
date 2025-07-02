8ed38d8d373adb35692140d6a6a2a468377381ea
yaswanth-16 <yaswanthvempuluru97@gmail.com>
2025-07-02 23:49:21 +0530

    Add index.js with addition of two numbers code

---
+++ index.js
@@ -0,0 +1,11 @@
+// index.js
+// Addition of two numbers
+
+function add(a, b) {
+    return a + b;
+}
+
+// Example usage
+const num1 = 5;
+const num2 = 7;
+console.log(`The sum of ${num1} and ${num2} is:`, add(num1, num2));
